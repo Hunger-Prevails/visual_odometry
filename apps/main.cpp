59b3c93963efd7110612272e0737366d21e80d5b
Hunger-Prevails <yinglun.liu.it.happens@gmail.com>
2025-12-19 18:56:34 +0100

    add cxxopts as dependency & add command line argument

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,9 +1,18 @@
 # include <iostream>
+# include <cxxopts.hpp>
 # include <Eigen/Dense>
 # include <Eigen/Core>
 # include <opencv2/opencv.hpp>
 # include <opencv2/core/eigen.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
+    cxxopts::Options options("visual-odometry", "options to configure visual odometry pipeline");
+
+	options.add_options()("sequence", "Path to the image sequence to process", cxxopts::value<std::string>());
+
+    auto args = options.parse(argc, argv);
+
+    std::cout << args["sequence"].as<std::string>() << std::endl;
+
     return 0;
 }
